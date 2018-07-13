35ee382318561e4807db433a45e3f22793ff575e
Fredrik Lack <fredrik@fre.la>
2018-07-13 23:09:37 +0200

    Using own namespace for ingress, also adding metrics and stats

--- local-exec/setup.sh
+++ local-exec/setup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 gcloud container clusters get-credentials $1 \
-	--zone $2 \
-	--project $3
+    --zone $2 \
+    --project $3
 
 kubectl create ns tiller
 kubectl create -f ./local-exec/setup.yaml
 
 helm init \
-	--service-account tiller \
-	--tiller-namespace tiller \
-	--wait
+    --service-account tiller \
+    --tiller-namespace tiller \
+    --wait
 
 # Install defaults
 helm install \
-	--namespace cert-manager \
-	--tiller-namespace tiller \
-	--name cert-manager \
-	stable/cert-manager
+    --namespace cert-manager \
+    --tiller-namespace tiller \
+    --name cert-manager \
+    stable/cert-manager
 
 helm install \
-	--tiller-namespace tiller \
-	--name nginx \
-	--set controller.service.loadBalancerIP=$4 \
-	--set controller.service.externalTrafficPolicy=Local \
+    --namespace nginx-ingress \
+    --tiller-namespace tiller \
+    --name nginx \
+    --set controller.service.loadBalancerIP=$4 \
+    --set controller.service.externalTrafficPolicy=Local \
+    --set controller.ingressClass=external \
+    --set controller.stats.enabled=true \
+    --set controller.metrics.enabled=true \
     stable/nginx-ingress
